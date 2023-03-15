c35a78e0cbe5e759dfc906699ff62c10579b6608
lucas-bertinchamp <lucas.bertinchamp@depinfonancy.net>
2023-03-15 22:43:24 +0100

    import

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
-import "@/styles/globals.css";
-import React from 'react';
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import "/styles/globals.css";
+import React from "react";
+import { UserProvider } from "@auth0/nextjs-auth0/client";
 
 export default function App({ Component, pageProps }) {
   return (
